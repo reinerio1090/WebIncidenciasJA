95bfd2d81fca9365df85cd1b2b4276dcb9fc4665
reinerio1090 <reinerio_rodriguez_1090@hotmail.com>
2024-05-26 13:30:49 -0500

    Update Navbar.jsx

--- src/Components/global_components/nabvar/Navbar.jsx
+++ src/Components/global_components/nabvar/Navbar.jsx
@@ -10,18 +10,19 @@ const Navbar = ({ changeMenu, activo }) => {
 
     return (
         <>
-            <div className={` ${open ? "w-72" : "w-20"} bg-dark-purple h-screen p-5 pt-8 relative duration-200 bg-gray-100`}>
-                <div className={`fixed left-0 pl-4 bg-gray-100 h-full duration-200 ${open ? "w-[255px]" : "w-[78px]"}`}>
-                    <div onClick={() => setOpen(!open)} className="flex gap-x-4 items-center">
-                        <img className="w-10" src="./img/dashboard/menu_icon.png"/>
-                        <h1 className={`text-black origin-left font-medium text-xl duration-200 ${!open && "scale-0"}`}>Incidencias JA</h1>
+            <div className={` ${open ? "w-72" : "w-20"} bg-dark-purple  py-8 relative duration-200 bg-gray-100`}>
+                <div className={`fixed left-0 pl-4  duration-200 ${open ? "w-[255px]" : "w-[78px]"}`}>
+                    <div onClick={() => setOpen(!open)} className="flex  items-center flex-col ">
+                        <img className="max-w-12 -ml-4" src="./img/dashboard/menu_icon.png"/>
+                        <h1 className={`text-black origin-left font-medium text-xl duration-200 ${!open && "scale-0"}`}>Incidencias J.A.</h1>
+                        <h1 className={`text-black origin-left font-light text-xs duration-200 ${!open && "scale-0"}`}>@ReinerioRodríguez</h1>
                     </div>
-                    <ul className="pt-6">
+                    <ul className="">
                         {Menus.map((Menu, index) => (
                             <li
                                 key={index}
                                 onClick={() => changeMenu(index)}
-                                className={`flex relative group  rounded-md p-2 cursor-pointer hover:bg-light-white text-black text-sm items-center gap-x-4  mt-2 ${index === 0 && "bg-light-white"} ${index==activo?"bg-gray-300":""} mr-4`}>
+                                className={`flex relative group  rounded-md p-2 cursor-pointer hover:bg-light-white text-black text-sm items-center gap-x-4  mt-2 ${index === 0 && "bg-light-white"} ${index==activo?"bg-gray-300":""} mr-4 `}>
                                 {(!open)&&<div className='absolute hidden group-hover:block  ml-12 rounded-md bg-green-900 p-2 text-white font-semibold min-w-36'>{Menu.title}</div>}
                                
                                 <span class={`${Menu.src} h-7 w-7 text-green-900`}></span>
